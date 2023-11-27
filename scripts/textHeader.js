19e8f36db786a77b9132a709669e46fd41f5e8a3
Zhekich123 <axelb380@gmail.com>
2023-11-27 11:54:16 -0800

    fixing bugs

--- scripts/textHeader.js
+++ scripts/textHeader.js
@@ -3,7 +3,7 @@
 const screenWidth = window.innerWidth;
 const headerText = document.querySelector('.header_text');
 
-if (screenWidth <=500) {
+if (screenWidth <=576) {
     headerText.innerHTML = 'Free GB <br/> for you'
 } else {
     headerText.innerHTML = 'Free GB for you'
